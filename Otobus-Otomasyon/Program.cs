84e9f53adee4a9905a5346c1eddc5aae0f7fc62e
Emin Kara <eminkral6534@gmail.com>
2024-12-31 20:49:45 +0300

    koltuk1 baslangıc formu yapıldı tekrardan

--- Otobus-Otomasyon/Program.cs
+++ Otobus-Otomasyon/Program.cs
@@ -16,7 +16,7 @@ namespace Otobus_Otomasyon
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Koltuk2());
+            Application.Run(new Koltuk1());
         }
     }
 }
