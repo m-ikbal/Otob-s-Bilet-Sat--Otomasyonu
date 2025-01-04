6e0fa4dc4de18aaa6ec0878753b549be1d1a4333
Emin Kara <eminkral6534@gmail.com>
2025-01-04 14:07:17 +0300

    Kullanıcı Ekle Form Boş Alan Kontrolleri Yapıldı

--- Otobus-Otomasyon/KullaniciEkle.cs
+++ Otobus-Otomasyon/KullaniciEkle.cs
@@ -21,26 +21,35 @@ namespace Otobus_Otomasyon
 
         private void btnKullaniciEkle_Click(object sender, EventArgs e)
         {
-            try
+            // bosalankontrol sınıfındaki fonksiyonu çağırıyoruz
+            if (bosalankontrol.AreFieldsValid(this))
             {
-                Kullanicilar kullanicilar = new Kullanicilar()
+                try
                 {
-                    kullaniciAd = txtKullaniciIsim.Text,
-                    kullaniciSoyad = txtKullaniciSoyisim.Text,
-                    kullaniciAdi = txtKullaniciAdi.Text,
-                    kullaniciSifre = txtKullaniciSifre.Text,
-                    kullaniciEposta = txtKullaniciEposta.Text,
-                    kullaniciRol = cmbKullaniciRol.Text
-                };
-                db.Kullanicilar.Add(kullanicilar);
-                db.SaveChanges();
-                MessageBox.Show("Kullanıcı başarıyla eklendi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            catch (Exception ex)
-            {
+                    Kullanicilar kullanicilar = new Kullanicilar()
+                    {
+                        kullaniciAd = txtKullaniciIsim.Text,
+                        kullaniciSoyad = txtKullaniciSoyisim.Text,
+                        kullaniciAdi = txtKullaniciAdi.Text,
+                        kullaniciSifre = txtKullaniciSifre.Text,
+                        kullaniciEposta = txtKullaniciEposta.Text,
+                        kullaniciRol = cmbKullaniciRol.Text
+                    };
+                    db.Kullanicilar.Add(kullanicilar);
+                    db.SaveChanges();
+                    MessageBox.Show("Kullanıcı başarıyla eklendi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
 
-                MessageBox.Show(ex.Message);
+                    MessageBox.Show(ex.Message);
+                }
             }
+            else
+            {
+                // Alanlar boşsa işlem yapılmaz
+            }
+            
         }
     }
 }
