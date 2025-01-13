fa80ed49ec8b2664e518adfdac8251126662b4c4
Emin Kara <eminkral6534@gmail.com>
2025-01-13 20:45:03 +0300

    Anasayfa Yazı Güncellendi

--- Otobus-Otomasyon/Anasayfa.cs
+++ Otobus-Otomasyon/Anasayfa.cs
@@ -22,7 +22,7 @@ namespace Otobus_Otomasyon
         private void Anasayfa_Load(object sender, EventArgs e)
         {
             ZamanTimer.Start();
-            lblKullaniciAdi.Text = $"User : {Session.KullaniciIsim}";
+            lblKullaniciAdi.Text = $"Hoş geldin {Session.KullaniciIsim}, Otobüs Bilet Otomasyonu'na";
         }
 
         private void ZamanTimer_Tick_1(object sender, EventArgs e)
